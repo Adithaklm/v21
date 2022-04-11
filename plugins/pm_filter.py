f7e7ef971611d982ab73f6f5d87acf06f89549ab
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-04-11 15:48:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -31,12 +31,10 @@ SPELL_CHECK = {}
 
 
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
-async def fil_mod(client, message):
-    k = await manual_filters(client, message)
-    if k == False:
-        await auto_filter(client, message)
-
-
+async def give_filter(client, message):
+    group_id = message.chat.id
+    name = message.text
+        
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
