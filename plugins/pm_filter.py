5b2ae892568226c5e63e1d2dee372ed068805a57
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-04-15 16:56:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -801,9 +801,9 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text"ᴘᴀɢᴇ", callback_data="pages"),
-             InlineKeyboardButton(text="1 - {round(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"next_{req}_{key}_{offset}")]
+            [InlineKeyboardButton(f"ᴘᴀɢᴇ", callback_data="pages"),
+             InlineKeyboardButton(text=f"1 - {round(int(total_results) / 10)}", callback_data="pages"),
+             InlineKeyboardButton(text=f"ɴᴇxᴛ", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
